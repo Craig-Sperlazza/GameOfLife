fe2427fd1497f857c47e09be4d375040d183db16
Craig Sperlazza <sperlazc@oregonstate.edu>
2023-02-28 13:59:12 -0800

    seperated packages into app, model, gui---MVC

--- src/application/App.java
+++ src/application/App.java
@@ -1,5 +1,7 @@
 package application;
 
+import gui.MainFrame;
+
 import javax.swing.*;
 
 public class App {
--- src/gui/GamePanel.java
+++ src/gui/GamePanel.java
@@ -1,4 +1,4 @@
-package application;
+package gui;
 
 import javax.swing.*;
 import java.awt.*;

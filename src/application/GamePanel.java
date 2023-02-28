acc62ebfb953288a92198206e332a0a0dc73a5c6
Craig Sperlazza <sperlazc@oregonstate.edu>
2023-02-28 12:53:14 -0800

    added inner and outer rectangles to panel

--- src/application/GamePanel.java
+++ src/application/GamePanel.java
@@ -4,17 +4,35 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GamePanel extends JPanel {
+
+    private final static int CELLSIZE = 50;
+
+    private final static Color backgroundColor = Color.BLACK;
+    private final static Color foregroundColor = Color.GREEN;
+    private final static Color gridColor = Color.GRAY;
+
+    private int topBottomMargin;
+    private int leftRightMargin;
+
+
     public GamePanel() {
     }
 
+    //note that swing calls this internally and checks for events. We do not call it anywhere
     @Override
     protected void paintComponent(Graphics g) {
-        Graphics2D g2 = (Graphics2D)g;
+        Graphics2D g2 = (Graphics2D) g;
 
         int width = getWidth();
         int height = getHeight();
 
-        g2.setColor(Color.GREEN);
+        leftRightMargin = ((width % CELLSIZE) + CELLSIZE) / 2;
+        topBottomMargin = ((height % CELLSIZE) + CELLSIZE) / 2;
+
+        g2.setColor(backgroundColor);
         g2.fillRect(0, 0, width, height);
+
+        g2.setColor(foregroundColor);
+        g2.fillRect(leftRightMargin, topBottomMargin, width - (2 * leftRightMargin), height - (2 * topBottomMargin));
     }
 }

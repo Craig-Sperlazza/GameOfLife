7e84ce162c6ec0cdd05190f5920ff4c9735b30de
Craig Sperlazza <sperlazc@oregonstate.edu>
2023-02-28 07:19:45 -0800

    added paint component to game panel

--- src/application/MainFrame.java
+++ src/application/MainFrame.java
@@ -1,11 +1,14 @@
 package application;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MainFrame extends JFrame {
+    private GamePanel gamePanel = new GamePanel();
     public MainFrame(){
         super("Game of Life");
-
+        setLayout(new BorderLayout());
+        add(gamePanel, BorderLayout.CENTER);
         setSize(1000, 800);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
